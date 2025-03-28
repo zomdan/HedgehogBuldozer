55655f0639229373dcad218ffe266dbb13032902
FTjahjana <tjahjanafrances@gmail.com>
2025-03-28 07:57:29 +0700

    testing with room graybox

---
+++ Assets/Code/sacambambaspis.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sacambambaspis : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0, moveZ) * speed * Time.deltaTime;
+        transform.Translate(move);
+    }
+    
+}
